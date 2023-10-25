a3bf4d2235120fbcb6fd0f1e8b81e20e00727d03
Logan Klumpp <klumpplogan@gmail.com>
2023-10-25 17:45:17 -0500

    Added Files

---
+++ init.lua
@@ -0,0 +1,30 @@
+require("lazyinit")
+pcall(require('telescope').load_extension, 'fzf')
+
+vim.cmd("colorscheme gruvbox")
+vim.cmd("set nu")
+vim.cmd("set rnu");
+
+vim.g.mapleader = " "
+--vim.cmd("map <Leader>fg :Telescope git_files<Esc>")
+--vim.cmd("map <Leader>ff :Telescope find_files<Esc>")
+
+vim.keymap.set('n', '<leader>fg', require('telescope.builtin').git_files, { desc = '[S]earch Git [F]iles' })
+vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+require("telescope").setup({
+	defaults = {
+		layout_config = {
+			horizontal = {
+				preview_cutoff = 0
+			}
+		}
+	}
+})
+require("nvim-treesitter.configs").setup({
+	ensure_installed = { "cpp" },
+	auto_install = true,
+	highlight = {
+		enable = true
+	}
+})
+require("mason").setup()
