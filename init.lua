1eb0911f66a4173f4d116cb4b5fa2dbdb58d11b8
Logan Klumpp <klumpplogan@gmail.com>
2023-10-26 20:06:36 -0500

    Added Find Buffer

--- init.lua
+++ init.lua
@@ -9,6 +9,7 @@ vim.g.mapleader = " "
 
 vim.keymap.set('n', '<leader>fg', require('telescope.builtin').git_files, { desc = '[S]earch Git [F]iles' })
 vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>fb', require('telescope.builtin').buffers, { desc = '' })
 require("telescope").setup({
 	defaults = {
 		layout_config = {
